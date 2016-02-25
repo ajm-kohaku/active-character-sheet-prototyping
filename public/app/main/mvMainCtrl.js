0de8eb4b5a5377e34507561b6ddef5b7bc73d257
Niertis Evani <niertis@gmail.com>
2016-02-24 22:18:15 -0500

    added additional statistics mapping.

--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -150,6 +150,18 @@ angular.module('app').controller('mvMainCtrl', function ($scope) {
                     value: 3,
                     comment: ""
                 }
+            ],
+            senseMotive: [
+                {
+                    type: "ranks",
+                    value: 16,
+                    comment: ""
+                },
+                {
+                    type: "trait",
+                    value: 1,
+                    comment: ""
+                }
             ]
         },
         armorClass: [
@@ -164,14 +176,6 @@ angular.module('app').controller('mvMainCtrl', function ($scope) {
             {
                 type: "deflection",
                 value: 5
-            },
-            {
-                type: "class",
-                value: 4
-            },
-            {
-                type: "wisdom",
-                value: 0
             }
         ],
         defenseStats: {
@@ -199,6 +203,55 @@ angular.module('app').controller('mvMainCtrl', function ($scope) {
                 }
             ]
         },
+        feats: [
+            {
+                type: "racial",
+                name: "Blood Drinker (Dhampir)",
+                description: "Choose one humanoid subtype. Whenever you drink fresh blood from the chosen type, gain 5 temporary hit points and +1 bonus on checks and saves based on Constitution. The effects last 1 hour. If you feed multiple times, you can continue to gain hit points to a max of 5 temporary hit points for every 3 HD you have, but the +1 to Con-based skill checks and saves does not stack.\nCan drink as part of a bite attack; drinking blood deals 2 points of Con damage to the creature you feed on. Feeding from unwilling intelligent creatures is an evil act."
+            },
+            {
+                type: "combat",
+                name: "Improved Unarmed Strike",
+                description: "You are considered to be armed even when unarmed—you do not provoke attacks of opportunity when you attack foes while unarmed. Your unarmed strikes can deal lethal or nonlethal damage, at your choice."
+            },
+            {
+                type: "general",
+                name: "Combat Casting",
+                description: "You get a +4 bonus on concentration checks made to cast a spell or use a spell-like ability when casting on the defensive or while grappled."
+            },
+            {
+                type: "style",
+                name: "Pummeling Style",
+                description: "Whenever you use a full-attack action or flurry of blows to make multiple attacks against a single opponent with unarmed strikes, total the damage from all hits before applying damage reduction. This ability works only with unarmed strikes, no matter what other abilities you might possess."
+            }
+        ],
+        traits: [
+            {
+                type: "combat",
+                name: "Reactionary",
+                description: "You gain a +2 trait bonus on initiative checks."
+            },
+            {
+                type: "drawback",
+                name: "Tainted Spirit",
+                description: "At the end of any combat, you must attempt a Fortitude saving throw. The DC is 10 + the number of rounds you acted in combat. If you fail this save, you become fatigued for 10 minutes per round you acted during that encounter."
+            },
+            {
+                type: "religious",
+                name: "Desperate Resolve",
+                description: "You gain a +1 trait bonus on concentration checks. This trait bonus increases to + 4 when you are grappled, pinned, in violent weather, or entangled."
+            },
+            {
+                type: "campaign",
+                name: "Outlander (Missionary)",
+                description: "You gain a +1 trait bonus on Knowledge (religion) checks, and Knowledge (religion) is a class skill for you. If you cast divine spells, pick three spells on your spell list. You are particularly adept at casting these spells, so they function at +1 caster level when you cast them, and their save DCs (if any) gain a +1 bonus."
+            },
+            {
+                type: "religious",
+                name: "Calistrian Prostitute",
+                description: "explaining how a prostitute can help you in social situations"
+            }
+        ],
         mythicInfo: {
             level: 3,
             mythicPath: "Champion",
@@ -276,10 +329,12 @@ angular.module('app').controller('mvMainCtrl', function ($scope) {
     function getAC() {
         var ac = getAbilityMod(dexterity);
         $.map(character.armorClass, function (value, index) {
-            if (value.type === "wisdom") {
-                ac += getAbilityMod(wisdom);
-            } else {
-                ac += value.value;
+            ac += value.value;
+        });
+        $.map(character.generalInfo.characterClasses, function (value, index) {
+            if (value.className === "Sacred Fist" || "Monk") {
+                // 1/4 Monk/Fist level + Wis Mod
+                ac += Math.floor(value.value/4) + getAbilityMod(wisdom);
             }
         });
         var size = getSizeMod();
@@ -343,7 +398,34 @@ angular.module('app').controller('mvMainCtrl', function ($scope) {
     }
 
     function getCmd() {
-        return null;
+        var sum = 10 + getBaseAttack() + getAbilityMod(strength) + getAbilityMod(dexterity) + getSizeMod();
+        $.map(character.generalInfo.characterClasses, function (value, index) {
+            if (value.className === "Sacred Fist" || value.className === "Monk") {
+                sum += getAbilityMod(wisdom)+Math.floor(value.value/4);
+            }
+        });
+        return sum;
+    }
+
+    function getAbilityList(obj) {
+        var parts = [];
+        $.map(obj, function(value, index){
+            parts.push(value.name);
+        });
+        return parts.join(", ");
+    }
+
+    function getSkillList() {
+        var parts = {};
+        $.map(character.skills, function(value, index) {
+            parts.push(value);
+        })
+    }
+
+    function splitAndCamelcase(str) {
+        str.replace(/^./, function(str){ return str.toUpperCase(); });
+        str.replace(/([A-Z])/g, ' $1');
+        return str;
     }
 
     $scope.init = function () {
@@ -363,8 +445,7 @@ angular.module('app').controller('mvMainCtrl', function ($scope) {
         $.map(character.senses, function (value, index) {
             parts.push(value.type);
         });
-        var combinedString = parts.join(", ");
-        return combinedString.substr(0, combinedString.length - 1);
+        return parts.join(", ");
     };
 
     $scope.perception = function () {
@@ -395,9 +476,7 @@ angular.module('app').controller('mvMainCtrl', function ($scope) {
     $scope.hitDie = function () {
         var generalInfo = this.character.generalInfo;
         var extraPoints = getStatTotal(generalInfo.characterClasses) * getAbilityMod(constitution);
-        console.log('my extrapoints are: ' + extraPoints);
         return character.defenseStats.hitPoints + " (" + getStatTotal(generalInfo.characterClasses) + character.defenseStats.hitDieType + "+" + extraPoints + ")";
-
     };
 
     $scope.strength = strength;
@@ -431,5 +510,19 @@ angular.module('app').controller('mvMainCtrl', function ($scope) {
 
     $scope.cmb = function () {
         return getCmb();
+    };
+
+    $scope.cmd = function () {
+        return getCmd();
+    };
+
+    $scope.feats = function() {
+      return getAbilityList(character.feats)
+    };
+    $scope.traits = function() {
+        return getAbilityList(character.traits)
+    };
+    $scope.skills = function() {
+        return getAbilityList(character.skills)
     }
 });
